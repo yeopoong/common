932e800474517c40c76c3e5cba975739a2b9be83
yeopoong <yeopoong@gmail.com>
2015-12-27 21:12:41 +0900

    first commit

---
+++ src/main/java/common/HttpClientTest.java
@@ -0,0 +1,70 @@
+package common;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+public class HttpClientTest {
+
+	public final static void main(String[] args) throws Exception {
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+		try {
+			HttpGet httpget = new HttpGet("http://httpbin.org/");
+
+			System.out.println("Executing request " + httpget.getRequestLine());
+
+			// Create a custom response handler
+			ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
+
+				@Override
+				public String handleResponse(final HttpResponse response) throws ClientProtocolException, IOException {
+					int status = response.getStatusLine().getStatusCode();
+					if (status >= 200 && status < 300) {
+						HttpEntity entity = response.getEntity();
+						return entity != null ? EntityUtils.toString(entity) : null;
+					} else {
+						throw new ClientProtocolException("Unexpected response status: " + status);
+					}
+				}
+
+			};
+			String responseBody = httpclient.execute(httpget, responseHandler);
+			System.out.println("----------------------------------------");
+			System.out.println(responseBody);
+		} finally {
+//			httpclient.close();
+		}
+
+		HttpPost httpPost = new HttpPost("http://targethost/login");
+
+		List<NameValuePair> nvps = new ArrayList<>();
+		nvps.add(new BasicNameValuePair("username", "vip"));
+		nvps.add(new BasicNameValuePair("password", "secret"));
+
+		httpPost.setEntity(new UrlEncodedFormEntity(nvps));
+
+		CloseableHttpResponse response2 = httpclient.execute(httpPost);
+
+		try {
+			System.out.println(response2.getStatusLine());
+			HttpEntity entity2 = response2.getEntity();
+			EntityUtils.consume(entity2);
+		} finally {
+			response2.close();
+		}
+	}
+}
